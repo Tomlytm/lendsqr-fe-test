db42ddc7ce4cce04692b28eee7af825de789204f
Tomlytm <Tomilayojesse@gmail.com>
2025-01-14 08:52:20 +0100

    added test files

--- src/pages/dashboard/users/user-detail.tsx
+++ src/pages/dashboard/users/user-detail.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import UserDetails from '../../../components/Dashboard/Users/UserDetails.tsx'
+import UserDetails from '../../../components/Dashboard/Users/UserDetails/UserDetails.tsx'
 
 function UserDetailPage() {
   return (
